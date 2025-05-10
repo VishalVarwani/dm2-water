46e106499d482f1ce322d6b687f9bda9ae65aa82
unknown <vishalvarwani8@gmail.com>
2025-05-10 10:27:47 +0200

    v

--- models/river_insights/environmental_health_score.sql
+++ models/river_insights/environmental_health_score.sql
@@ -2,10 +2,9 @@
 
 SELECT
   DATE(Timestamp) AS date,
-  ROUND(AVG(Dissolved_Oxygen), 2) AS avg_do,
+  ROUND(AVG(`Dissolved Oxygen`), 2) AS avg_do,
   ROUND(AVG(pH), 2) AS avg_ph,
   ROUND(AVG(Turbidity), 2) AS avg_turbidity,
-  -- Example scoring: higher DO and pH around 7 is good, lower turbidity is good
   ROUND(
     0.4 * AVG(`Dissolved Oxygen`) +
     0.3 * (7 - ABS(7 - AVG(pH))) +
